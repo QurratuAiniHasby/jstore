cab76430ffacd4a87df66baf3ada876625a783cd
Qurratu Aini Hasby <qurratu.aini@ui.ac.id>
2019-02-21 16:43:56 +0700

    Initial commit

---
+++ Supplier.java
@@ -0,0 +1,92 @@
+
+/**
+ * Write a description of class Supplier here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Supplier
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private char name;
+    private char email;
+    private char phoneNumber;
+    private char city;
+
+    /**
+     * Constructor for objects of class Supplier
+     */
+    public Supplier()
+    {
+        // initialise instance variables
+        id = 0;
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getId()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    public char getName()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    public char getEmail()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    public char getPhoneNumber()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    public char getCity()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    public int setId()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    public char setName()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    public char setEmail()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    public char setPhoneNumber()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    public setCity()
+    {
+        // put your code here
+        return 0;
+    }
+}
